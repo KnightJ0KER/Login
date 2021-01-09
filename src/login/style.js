abb620fefd9d9b376dcb449821141e55b79964b4
KnightJ0KER <71023499+KnightJ0KER@users.noreply.github.com>
2021-01-09 10:49:45 +0530

    Add files via upload

---
+++ src/login/style.js
@@ -0,0 +1,62 @@
+const React = require("react-native");
+
+const { StyleSheet } = React;
+
+export default {
+
+containerView: {
+  flex: 1,
+},
+loginScreenContainer: {
+  flex: 1,
+  backgroundColor:'#cae4db'
+},
+logoText: {
+  fontSize: 40,
+  fontWeight: "800",
+  marginTop: 150,
+  marginBottom: 30,
+  textAlign: 'center',
+  color: '#184d47'
+},
+loginFormView: {
+  flex: 1
+},
+loginFormTextInput: {
+  height: 43,
+  fontSize: 14,
+  borderRadius: 3,
+  borderWidth: 1,
+  borderColor: '#eaeaea',
+  backgroundColor: '#fafafa',
+  paddingLeft: 10,
+  marginLeft: 15,
+  marginRight: 15,
+  marginTop: 5,
+  marginBottom: 5,
+  textAlign:'center'
+
+},
+loginButton: {
+  backgroundColor: '#00917c',
+  borderRadius: 5,
+  height: 45,
+  marginTop: 10,
+  width:200,
+  marginLeft:'25%'
+},
+signupButton: {
+
+  backgroundColor: '#00917c',
+  borderRadius: 5,
+  height: 45,
+  marginTop: 50,
+  width:200,
+  marginLeft:'25%'
+},
+
+forgotPass: {
+  marginTop: 10,
+  marginLeft:200
+},
+};
